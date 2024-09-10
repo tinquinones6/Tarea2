809b84a02e284184e40acbb0d68a0dbf0949ec70
Martín Quiñones Manent <martin.quinones2001lumnos.ubiobio.cl>
2024-09-10 00:11:59 -0300

    ADD archivo con operaciones de condicionales, antes tuve un error ya que no guarde los archivos antes de subirlos, por lo que los commit quedaron diferentes

---
+++ Condicionales.js
@@ -0,0 +1,21 @@
+const numero1 = 10;
+const numero2 = 20;
+const numero3 = 30;
+
+const mayor = Math.max(numero1, numero2, numero3);
+console.log("El número mayor es:", mayor);
+
+const menor = Math.min(numero1, numero2, numero3);
+console.log("El número menor es:", menor);
+
+console.log("El número 1 es", numero1 % 2 === 0 ? "par" : "impar");
+
+console.log("El número 2 es", numero2 % 2 === 0 ? "par" : "impar");
+
+console.log("El número 3 es", numero3 % 2 === 0 ? "par" : "impar");
+
+console.log("El número 1", numero1 % 5 === 0 ? "es múltiplo de 5" : "no es múltiplo de 5");
+
+console.log("El número 2", numero2 % 5 === 0 ? "es múltiplo de 5" : "no es múltiplo de 5");
+
+console.log("El número 3", numero3 % 5 === 0 ? "es múltiplo de 5" : "no es múltiplo de 5");
