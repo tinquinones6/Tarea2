8176d6738f8ccdd87649c043f7f02007d5b41b8e
Martín Quiñones Manent <martin.quinones2001lumnos.ubiobio.cl>
2024-09-10 00:27:11 -0300

    ADD archivo con ejercicios de creación de objetos

---
+++ Objetos.js
@@ -0,0 +1,35 @@
+const persona = {
+    nombre: "Martín Quiñones",
+    edad: 22,
+    genero: "Masculino"
+  };
+  
+  console.log(persona);
+  console.log(persona.nombre);
+  console.log(persona.edad);
+  console.log(persona.genero);
+  console.log(typeof persona);
+  console.log(typeof persona.nombre);
+  console.log(typeof persona.edad);
+  console.log(typeof persona.genero);
+  
+  const caja = {
+    cuadernos: 5,
+    lapices: 10,
+    papel: 50,
+    fotografias: 3,
+    buenEstado: true
+  };
+  
+  console.log(caja);
+  console.log(caja.cuadernos);
+  console.log(caja.lapices);
+  console.log(caja.papel);
+  console.log(caja.fotografias);
+  console.log(caja.buenEstado);
+  console.log(typeof caja);
+  console.log(typeof caja.cuadernos);
+  console.log(typeof caja.lapices);
+  console.log(typeof caja.papel);
+  console.log(typeof caja.fotografias);
+  console.log(typeof caja.buenEstado);
